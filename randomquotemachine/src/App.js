584bf53c751cd0912bbd022ce919a88277cfc630
brandon quezada <brandon@sardius.media>
2020-04-30 14:36:25 -0500

    added user experiance test for freecodecamp and am passing the first test

--- randomquotemachine/src/App.js
+++ randomquotemachine/src/App.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" id="quote-box">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>this is sparta!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
